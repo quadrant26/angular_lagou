268628046ae965bfdc4a702c316af8cf1638c9d5
quadrant26 <quadrant26@163.com>
2017-11-22 11:05:49 +0800

    add scope list

--- src/script/controller/mainCtrl.js
+++ src/script/controller/mainCtrl.js
@@ -1,5 +1,25 @@
 'use strict';
 
 angular.module('app').controller('mainCtrl', ['$http', '$scope', function ($http, $scope){
-
+    // $scope.abc = '123';
+    $scope.list = [
+        {
+            id: '1',
+            name: '销售',
+            imgSrc: 'image/company-3.png',
+            companyName: '千度',
+            city: '上海',
+            industry: '互联网',
+            time: '2017-06-01 11:05'
+        },
+        {
+            id: '2',
+            name: 'Web前端',
+            imgSrc: 'image/company-1.png',
+            companyName: '慕课',
+            city: '上海',
+            industry: '互联网',
+            time: '2017-06-01 11:05'
+        }
+    ]
 }])
