26f2feef058630811b5f59af7f20cf0cc72dda4a
quadrant26 <quadrant26@163.com>
2017-11-23 14:54:01 +0800

    $http company.json

--- src/script/controller/companyCtrl.js
+++ src/script/controller/companyCtrl.js
@@ -1,5 +1,12 @@
 'use strict';
 
-angular.module('app').controller('companyCtrl', ['$scope', function ($scope){
-    
+angular.module('app').controller('companyCtrl', ['$http', '$state', '$scope', function ($http, $state, $scope){
+    $http.get('/data/company.json', {
+        params : {
+            id: $state.params.id
+        }
+    }).success(function (resq){
+        console.log(resq);
+        $scope.company = resq;
+    });
 }])
