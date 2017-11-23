e713fbbd0b1b795522991338702648aca516409d
quadrant26 <quadrant26@163.com>
2017-11-23 14:35:05 +0800

    $http position.json

--- src/script/controller/positionCtrl.js
+++ src/script/controller/positionCtrl.js
@@ -1,5 +1,39 @@
 'use strict';
 
-angular.module('app').controller('positionCtrl', ['$scope', function ($scope){
+angular.module('app').controller('positionCtrl', ['$q', '$http', '$state', '$scope', function ($q, $http, $state, $scope){
+    // console.log($state);
+    $scope.isLogin = false;
 
+    $http.get("/data/position.json?id=" + $state.params.id).success(function (resq){
+        console.log(resq);
+        $scope.position = resq;
+    })
+
+    // function getPosition(){
+    //     var def = $q.defer();
+    //     $http.get('/data/position.json', {
+    //         params: {
+    //             id: $state.params.id
+    //         }
+    //     }).success(function (resq){
+    //         console.log(resq);
+    //         $scope.position = resq;
+    //         def.resolve(resq);
+    //     }).error(function (resq){
+    //         def.reject(err);
+    //     });
+
+    //     return def.promise;
+    // }
+
+    // function getCompany(id) {
+    //     $http.get('/data/company.json?id=' + id).success( function (resq){
+    //         $scope.company = resq;
+    //     })
+    // }
+
+    // getPosition().then( function (obj){
+    //     getCompany(obj.companyId)
+    // })
+    
 }])
