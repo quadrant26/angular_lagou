494b7b94875e98d824346d53cefee2c2f0ba03a0
quadrant26 <quadrant26@163.com>
2017-11-24 11:16:05 +0800

    注释console

--- src/script/controller/companyCtrl.js
+++ src/script/controller/companyCtrl.js
@@ -1,12 +1,14 @@
 'use strict';
 
 angular.module('app').controller('companyCtrl', ['$http', '$state', '$scope', function ($http, $state, $scope){
+
     $http.get('/data/company.json', {
         params : {
             id: $state.params.id
         }
     }).success(function (resq){
-        console.log(resq);
+        // console.log(resq);
         $scope.company = resq;
     });
+    
 }])
