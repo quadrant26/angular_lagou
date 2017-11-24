0d66f531775644c0d1ef3bda615c7edff4572f1d
quadrant26 <quadrant26@163.com>
2017-11-24 11:16:33 +0800

    tab showPositionList and $watch

--- src/script/directive/positionClass.js
+++ src/script/directive/positionClass.js
@@ -7,6 +7,17 @@ angular.module('app').directive('appPositionClass', [function (){
         templateUrl: 'view/template/positionClass.html',
         scope: {
             com: "="
+        },
+        link : function ($scope) {
+            $scope.showPositionList = function (idx){
+                // console.log($scope.com);
+                $scope.positionList = $scope.com.positionClass[idx].positionList;
+                $scope.isActive = idx;
+            };
+
+            $scope.$watch('com', function (newVal){
+                if(newVal) $scope.showPositionList(0);
+            });
         }
     }
 }])
