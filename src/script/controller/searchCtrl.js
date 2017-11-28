f56fb3885c16ba30d0759d36b4941233258dbf4a
quadrant26 <quadrant26@163.com>
2017-11-28 11:52:24 +0800

    add select click method

--- src/script/controller/searchCtrl.js
+++ src/script/controller/searchCtrl.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('app').controller('searchCtrl', ['$http', '$scope', function ($http, $scope){
+angular.module('app').controller('searchCtrl', ['dict', '$http', '$scope', function (dict, $http, $scope){
     
 
     $scope.name = '';
@@ -21,7 +21,43 @@ angular.module('app').controller('searchCtrl', ['$http', '$scope', function ($ht
     $scope.tabList = [
         {id: 'city', name: '城市'},
         {id: 'salary', name: '薪水'},
-        {id: 'sacle', name: '公司规模'}
-    ]
+        {id: 'scale', name: '公司规模'}
+    ];
+    $scope.sheet = {};
+
+    var tabId = '';
+    $scope.tClick = function (id, name){
+        tabId = id;
+        $scope.sheet.list = dict[id];
+        $scope.sheet.visiable = true;
+    }
+
+    // 操作下拉列表
+    $scope.sClick = function (id, name){
+        // 根据下拉选项修改标题名称
+        if( id ){
+            angular.forEach($scope.tabList, function (item){
+                if( item.id === tabId){
+                    item.name = name;
+                }
+            });
+        }else{
+            angular.forEach($scope.tabList, function (item){
+                if( item.id === tabId ){
+                    switch(item.id){
+                        case 'city':
+                            item.name = '城市';
+                            break;
+                        case 'salary':
+                            item.name = '薪水';
+                            break;
+                        case 'scale':
+                            item.name = '公司规模';
+                            break;
+                    }
+                }
+            })
+        };
+    }
 
 }])
