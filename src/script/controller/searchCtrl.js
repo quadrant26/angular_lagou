73d53a61630694c219b434e097b9388ccf7d4712
quadrant26 <quadrant26@163.com>
2017-11-28 11:07:32 +0800

    tablist and normal method

--- src/script/controller/searchCtrl.js
+++ src/script/controller/searchCtrl.js
@@ -1,7 +1,27 @@
 'use strict';
 
 angular.module('app').controller('searchCtrl', ['$http', '$scope', function ($http, $scope){
-    $http.get('data/positionList.json').success(function (resq){
-        $scope.list = resq;
-    })
+    
+
+    $scope.name = '';
+    $scope.visiable = true;
+    $scope.search = function (){
+        $http.get('data/positionList.json', {
+            params: {
+                name : $scope.name
+            }
+        }).success(function (resq){
+            $scope.list = resq;
+        });
+    }
+
+    // 初始化搜索结果
+    $scope.search();
+
+    $scope.tabList = [
+        {id: 'city', name: '城市'},
+        {id: 'salary', name: '薪水'},
+        {id: 'sacle', name: '公司规模'}
+    ]
+
 }])
