2b3a44c6906bc5e5146732740060cb7b816acf3f
quadrant26 <quadrant26@163.com>
2017-11-30 11:13:42 +0800

    获取投递记录和过滤筛选

--- src/script/controller/postCtrl.js
+++ src/script/controller/postCtrl.js
@@ -6,4 +6,26 @@ angular.module('app').controller('postCtrl', ['$http', '$scope', function ($http
         {"id": "pass", name: "面试邀请"},
         {"id": "fail", name: "不合格"},
     ];
+
+    $http.get('/data/myPost.json').success(function (resq){
+        $scope.list = resq;
+        console.log(resq);
+    })
+
+    $scope.filterByObj = {};
+
+    $scope.tClick = function (id, name){
+
+        switch(id){
+            case 'all':
+                delete $scope.filterByObj.state;
+                break;
+            case 'pass':
+                $scope.filterByObj.state = '1';   
+                break;
+            case 'fail':
+                $scope.filterByObj.state = '-1';   
+                break;
+        }
+    }
 }])
