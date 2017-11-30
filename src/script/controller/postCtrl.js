65ae07eb0c086e9e34714e26e193f42a98f38566
quadrant26 <quadrant26@163.com>
2017-11-30 10:56:23 +0800

    post tabList

--- src/script/controller/postCtrl.js
+++ src/script/controller/postCtrl.js
@@ -1,5 +1,9 @@
 'use strict';
 
 angular.module('app').controller('postCtrl', ['$http', '$scope', function ($http, $scope){
-
+    $scope.tabList = [
+        {"id": "all", name: "所有"},
+        {"id": "pass", name: "面试邀请"},
+        {"id": "fail", name: "不合格"},
+    ];
 }])
