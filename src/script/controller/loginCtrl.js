2fb721e3ca0758cce3dfd9da9c5429eadfe8c708
quadrant26 <quadrant26@163.com>
2017-12-04 14:00:29 +0800

    login ngSubmit

--- src/script/controller/loginCtrl.js
+++ src/script/controller/loginCtrl.js
@@ -1,5 +1,14 @@
 'use strict';
 
-angular.module('app').controller('loginCtrl', ['$http', '$scope', function ($http, $scope){
-
+angular.module('app').controller('loginCtrl', ['$http', '$scope', '$state', 'cache', function ($http, $scope, $state, cache){
+    $scope.submit = function (){
+        $http.post('/data/login.json', $scope.user).success(function (resq){
+            if(resq){
+                cache.put('id', resq.id);
+                cache.put('name', resq.name);
+                cache.put('image', resq.image);
+                $state.go('main');
+            }
+        });
+    }
 }])
