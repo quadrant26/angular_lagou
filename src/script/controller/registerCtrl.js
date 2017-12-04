de56392447440e774216c16a7e72bafbb77a11bc
quadrant26 <quadrant26@163.com>
2017-12-04 12:05:37 +0800

    registerCtrl sumit and send $interval

--- src/script/controller/registerCtrl.js
+++ src/script/controller/registerCtrl.js
@@ -1,5 +1,35 @@
 'use strict';
 
-angular.module('app').controller('registerCtrl', ['$http', '$scope', function ($http, $scope){
+angular.module('app').controller('registerCtrl', ['$interval', '$http', '$scope', '$state', function ($interval, $http, $scope, $state){
+
+    // 提交注册
+    $scope.submit = function (){
+        $http.post('/data/regist.json', $scope.user).success(function (resq){
+
+            if(resq){
+                $state.go('login');
+            }
+        })
+    };
+
+    var count = 60;
+    
+    $scope.send = function (){
+        $http.get('/data/code.json').success(function (resq){
+            if(resq.state === 1){
+                count = 60;
+                $scope.time = '60s';
+                var interval = $interval(function (){
+                    count --;
+                    if( count <= 0){
+                        $interval.cancel(interval);
+                        $scope.time = '';
+                    }else{
+                        $scope.time = count + 's';
+                    }
+                }, 1000);
+            }
+        })
+    }
 
 }])
