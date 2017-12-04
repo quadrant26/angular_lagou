2e08f35ad4678d1cfe7bd82006b72902120ed7bd
quadrant26 <quadrant26@163.com>
2017-12-04 10:58:04 +0800

    .pipe($.plumber())

--- gulpfile.js
+++ gulpfile.js
@@ -31,6 +31,7 @@ gulp.task('json', function (){
 
 gulp.task('less', function (){
     gulp.src(app.srcPath + 'style/index.less')
+    .pipe($.plumber())
     .pipe($.less()) // 编译 less
     .pipe(gulp.dest( app.devPath + 'css' )) 
     .pipe($.cssmin())       // 压缩
@@ -40,6 +41,7 @@ gulp.task('less', function (){
 
 gulp.task('js', function (){
     gulp.src(app.srcPath + 'script/**/*.js')
+    .pipe($.plumber())
     .pipe($.concat('index.js')) // 合并js
     .pipe(gulp.dest(app.devPath + 'js'))
     .pipe($.uglify()) // js 压缩
@@ -49,6 +51,7 @@ gulp.task('js', function (){
 
 gulp.task('image', function (){
     gulp.src(app.srcPath + 'image/**/*')
+    .pipe($.plumber())
     .pipe(gulp.dest(app.devPath + 'image'))
     .pipe($.imagemin()) // 图片压缩
     .pipe(gulp.dest(app.prdPath + 'image'))
