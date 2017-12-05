742f4953b62c1213384a9f4bbea81935e62d03af
quadrant26 <quadrant26@163.com>
2017-12-05 11:29:55 +0800

    change cache get methods

--- src/script/service/cache.js
+++ src/script/service/cache.js
@@ -1,30 +1,29 @@
 'use strict';
 
 angular.module('app')
-// .service('cache', ['$cookies', function ($cookies){
-//     this.put = function (key, value){
-//         $cookies.put(key, value);
-//     };
+.service('cache', ['$cookies', function ($cookies) {
+    this.put = function (key, value) {
+        $cookies.put(key, value);
+    };
 
-//     this.get = function (key){
-//         return $cookies.put(key);
-//     };
+    this.get = function (key) {
+        return $cookies.get(key);
+    };
 
-//     this.remove = function (key){
-//         $cookies.remove(key)
+    this.remove = function (key) {
+        $cookies.remove(key)
+    };
+}])
+// .factory('cache', ['$cookies', function ($cookies){
+//     return {
+//         put : function (key, value){
+//             $cookies.put(key, value);
+//         },
+//         get : function (key){
+//             return $cookies.gett(key)
+//         },
+//         remove: function (key){
+//             $cookies.remove(key);
+//         }
 //     }
 // }])
-.factory('cache', ['$cookies', function ($cookies){
-    return {
-        put : function (key, value){
-            $cookies.put(key, value);
-        },
-        get : function (key){
-            return $cookies.put(key)
-        },
-        remove: function (key){
-            $cookies.remove(key);
-        }
-    }
-}])
-
