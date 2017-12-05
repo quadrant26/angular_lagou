09c43daf61ee68d644329f6edfc47f8d3a978bb8
quadrant26 <quadrant26@163.com>
2017-12-05 15:31:10 +0800

    获取收藏记录

--- src/script/controller/favoriteCtrl.js
+++ src/script/controller/favoriteCtrl.js
@@ -1,5 +1,7 @@
 'use strict';
 
 angular.module('app').controller('favoriteCtrl', ['$http', '$scope', function ($http, $scope){
-
+    $http.get('/data/MyFavorite.json').success(function (resq){
+        $scope.list = resq;
+    });
 }])
